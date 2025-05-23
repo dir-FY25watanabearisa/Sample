04e93fc8eaf70a2bf9877142cbf228b773b1825f
arisa.watanabe <arsia.watanabe.x198@dirbato.co.jp>
2025-05-23 12:13:04 +0900

    2-2

--- Sample/src/sample/Test.java
+++ Sample/src/sample/Test.java
@@ -2,11 +2,13 @@ package sample;
 
 public class Test {
 
+    // mainメソッドはプログラムの最初に動作します。
     public static void main(String[] args) {
+        /* コメントアウト
         System.out.println("Kiramex");
         System.out.println("Java");
-        System.out.println("学習を始めましょう！");
-
+        */
+        System.out.println("学習を始めましょう!");
     }
 
 }
